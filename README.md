fc0ebe0b24a19f2cea8d090aebda7310422cbfad
Adrian Hofer <a1209967@univie.ac.at>
2023-07-05 10:53:53 +0200

    v1

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # telegram-bot-emo-support
+
+## Getting Started 
+
+Create a virtual python environment
+```console
+python -m venv .venv
+```
+Activate the python environment. 
+```console
+.\.venv\Scripts\activate
+```
+Install the requirements into the environment.
+```console
+pip install -r .\requirements.txt
+```
+Run the bot.py
+```console
+python .\src\bot.py
+```
+
+Other options: use a single python instance or use Anaconda
+
+## BotFather
+Use https://t.me/BotFather to create and manage your Telegram bots.
+You get your Telegram API Token from BotFather.
+
+## Environemt Variables
+### TELEGRAM_API_TOKEN
+API token for Telegram obtained by BotFather.
+### EMO_SUPPORT_GROUP_ID
+Id of the Telgram group receiving the bot's messages.
---
+++ src/bot.py
@@ -0,0 +1,104 @@
+'''
+Telegram bot to automatically send messages to emo support group
+'''
+import logging
+import os
+# pylint: disable=import-error
+from dateutil import tz
+from telegram import Update
+from telegram.ext import filters, MessageHandler, CommandHandler, ApplicationBuilder, ContextTypes
+
+DATE_FORMAT  = "%m/%d/%Y %H:%M:%S"
+RESPONSE_MESSAGE_FILE = "response-message.txt"
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def _read_response_message() -> str:
+    with open(RESPONSE_MESSAGE_FILE, "r", encoding="utf-8") as file:
+        return file.read()
+
+def _set_response_message(response_message) -> None:
+    with open(RESPONSE_MESSAGE_FILE, "w", encoding="utf-8") as file:
+        return file.write(response_message)
+
+async def private_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """A private message is received. Send a message to the group."""
+
+    local_zone = tz.tzlocal()
+    dt_local = update.effective_message.date.astimezone(local_zone)
+    dt_format = dt_local.strftime(DATE_FORMAT)
+
+    await context.bot.send_message(
+        chat_id=os.environ["EMO_SUPPORT_GROUP_ID"],
+        text=f"""Message metadata
+
+Firstname: *{update.effective_user.first_name}*
+Lastname: *{update.effective_user.last_name}*
+Username: [{update.effective_user.username}](https://t.me/{update.effective_user.username})
+Date: {dt_format}
+        
+
+
+Original Message        
+
+{update.effective_message.text}""",
+        parse_mode="markdown",
+    )
+
+    await context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text=_read_response_message()
+    )
+
+async def get_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Send the current response message.
+    """
+    await context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text="Current response message: " +_read_response_message()
+    )
+
+async def set_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    Set the current response message to the text of the message except the command.
+    """
+    messsage = update.effective_message.text[12:]
+    _set_response_message(messsage)
+    await context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text="Set response message to: " + messsage
+    )
+
+def main():
+    """
+    Create the app.
+    Add Handlers.
+    Run Polling for messages.
+    """
+    app = ApplicationBuilder().token(os.environ['TELEGRAM_API_TOKEN']).build()
+
+    private_message_handler = MessageHandler(
+        filters.TEXT &
+        (~filters.COMMAND) &
+        filters.ChatType.PRIVATE, private_message
+    )
+    get_message_handler = CommandHandler(
+        "get_message", get_message
+    )
+    set_message_handler = CommandHandler(
+        "set_message", set_message
+    )
+
+    app.add_handler(private_message_handler)
+    app.add_handler(get_message_handler)
+    app.add_handler(set_message_handler)
+
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == '__main__':
+    main()
